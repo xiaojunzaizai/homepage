ee9d8868835eef706172a1a990aca21d48a5912f
xiaojunzaizai <xiaojunzaizai@gmail.com>
2023-12-17 22:12:18 -0500

    test

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -7,31 +7,7 @@ import { Component } from '@angular/core';
 })
 export class TopBarComponent {
 
-  check(){
-    interface Institution {
-      name: string;
-      id: number;
-    }
-    
-    interface ListElement {
-      Institutions: Institution[];
-      name: string;
-      mailAddress: object;
-      SiteNumber: number;
-    }
 
-    let lists : ListElement[] =[
-      { Institutions: [{name:'World1',id:1},{name:'World1-2',id:2}],name:' Hello1',mailAddress:{},SiteNumber:1},
-      {Institutions: [{name:'world2-1', id:1},{name:'world2-2',id:2},{name:'world2-3', id:3}],name:' Hello2',mailAddress:{},SiteNumber:2},
-      {Institutions: [{name:'world3-1',id:1},{name:'world3-2',id:2}],name:' Hello3',mailAddress:{},SiteNumber:3}
-    ];
-
-    let newList: Institution[];
-    newList = lists.reduce((accumulator: any[], currentValue) => {
-      return accumulator.concat(currentValue.Institutions);
-    }, []);
-    console.log(newList);
-  }
 }
 
 
